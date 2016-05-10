161bdcb84ef9f03dd08a4399bdfecc280c306ee2
haad <haad@headbanggames.com>
2016-05-10 18:23:04 +0200

    Keep the whole log entry in the index instead of the payload

--- src/FeedIndex.js
+++ src/FeedIndex.js
@@ -8,7 +8,7 @@ class FeedIndex extends EventIndex {
       if(handled.indexOf(item.hash) === -1) {
         handled.push(item.hash);
         if(item.payload.op === 'ADD') {
-          this._index[item.hash] = item.payload
+          this._index[item.hash] = item
         } else if(item.payload.op === 'DEL') {
           delete this._index[item.payload.value];
         }
