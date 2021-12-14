62fcc25dec2b51a16f39a4aaecc260ba3779e489
julienmalard <julien.malard@mail.mcgill.ca>
2021-12-14 12:01:12 +0100

    First proposal of injected EventStore

--- src/FeedStore.js
+++ src/FeedStore.js
@@ -1,28 +1,31 @@
 'use strict'
 
-const EventStore = require('orbit-db-eventstore')
 const FeedIndex  = require('./FeedIndex')
 
-class FeedStore extends EventStore {
-  constructor (ipfs, id, dbname, options) {
-    if(!options) options = {}
-    if(!options.Index) Object.assign(options, { Index: FeedIndex })
-    super(ipfs, id, dbname, options)
-    this._type = 'feed'
-  }
-
-  remove (hash, options = {}) {
-    return this.del(hash, options)
-  }
-
-  del (hash, options = {}) {
-    const operation = {
-      op: 'DEL',
-      key: null,
-      value: hash
+const genFeedStore = function(EventStore) {
+  class FeedStore extends EventStore {
+    constructor (ipfs, id, dbname, options) {
+      if(!options) options = {}
+      if(!options.Index) Object.assign(options, { Index: FeedIndex })
+      super(ipfs, id, dbname, options)
+      this._type = 'feed'
     }
-    return this._addOperation(operation, options)
+
+    remove (hash, options = {}) {
+      return this.del(hash, options)
+    }
+
+    del (hash, options = {}) {
+      const operation = {
+        op: 'DEL',
+        key: null,
+        value: hash
+      }
+      return this._addOperation(operation, options)
+    }
   }
+  return FeedStore
 }
 
-module.exports = FeedStore
+
+module.exports = genFeedStore
